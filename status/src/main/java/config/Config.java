31ceaecf7bc2257167cb17466de900c0aa321fd1
unknown <lassehansencphbusiness@gmail.com>
2018-10-29 17:20:10 +0100

    add status

---
+++ status/src/main/java/config/Config.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author Lasse
+ */
+public class Config {
+
+    public static final String STATUS_FILE
+            = "C:\\Users\\Lasse\\Downloads"
+            + "\\wildfly-11.0.0.Final\\wildfly-11.0.0.Final\\standalone"
+            + "\\deployments\\status.txt";
+}
---
+++ status/src/main/java/resources/GenericResource.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package resources;
+
+import config.Config;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author Lasse
+ */
+@Path("/")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getStatus() throws FileNotFoundException, IOException {
+        String content = new String(
+                Files.readAllBytes(Paths.get(Config.STATUS_FILE)),
+                StandardCharsets.UTF_8);
+
+        return Response.ok(content).build();
+    }
+}
