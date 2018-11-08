e2f5fe7c3d128e879f3725b783e0314e5b7c3933
unknown <lassehansencphbusiness@gmail.com>
2018-11-08 16:37:27 +0100

    Add script to poll latency

---
+++ misc/poll.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import requests
+import time
+
+file_latencies = "latencies.pkl"
+file_graph = "latencies.png"
+
+latencies = defaultdict(lambda: 0)
+
+def measure():
+	headers = {"connection": "close"}
+	r = requests.get("http://localhost:8080/latest/frontpage")
+	status = r.status_code
+	ms = int(r.elapsed.total_seconds() * 1000)
+	latencies[ms] += 1
+
+def draw():
+	global file_graph
+	plt.bar(list(latencies.keys()), latencies.values(), color="g", alpha=0.5)
+	plt.ylabel("Amount in bucket")
+	plt.title("Latency of GET /latest/frontpage in ms")
+	plt.savefig(file_graph)
+
+while True:
+	measure()
+	draw()
+	time.sleep(30)
+
